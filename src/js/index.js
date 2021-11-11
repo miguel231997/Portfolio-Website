47856d2bea69fc749a8f7b69a3ee65b55e48602a
Miguel Galindo <miguelgalindo@new-host-2.home>
2021-11-11 00:22:40 -0500

    Finished Portfolio Website:

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import initTilt from "./tilt";
+import initSr from "./sr";
+import { addResume } from "./utils";
+import resume from "../assets/Galindo_Miguel_Resume_2021.pdf";
+
+export default function initApp() {
+  initSr();
+  initTilt();
+  // comment this if you don't want to attach your resume
+  addResume(resume);
+}
