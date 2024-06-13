44b76d72f614ea0b15c03a913f2ce40ea83b5b7b
Miguel Galindo <miguelgalindo1214@gmail.com>
2024-06-13 12:56:09 -0400

    Updating production dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const { merge } = require("webpack-merge");
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
@@ -15,7 +15,7 @@ module.exports = merge(common, {
   },
   optimization: {
     minimizer: [
-      new OptimizeCssAssetsPlugin(),
+      new CssMinimizerPlugin(),
       new TerserPlugin(),
       new HtmlWebpackPlugin({
         template: "./src/template.html",
